96ccd601481a6dfc9b7fd5cf9bcbbcf6871006e4
SamE-345 <sameveritt66@gmail.com>
2024-12-19 21:00:52 +0000

    Started a basic projectile simulator

---
+++ Visual_Simulation/Visual_Simulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Visual_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
