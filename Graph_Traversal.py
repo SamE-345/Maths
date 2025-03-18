c90773e59a16dcc586d9c6a94bc9be4ff911d6ea
SamE-345 <sameveritt66@gmail.com>
2025-03-18 14:23:23 +0000

    Animated the graph traversal

--- Graph_Traversal.py
+++ Graph_Traversal.py
@@ -2,32 +2,46 @@ from manim import *
 
 class node:
     def __init__(self, value):
-        self.rn = -1 ##Right child node
-        self.ln = -1 ##Left child node value
+        self.rn = None ##Right child node
+        self.ln = None ##Left child node value
         self.val = value ##Value of this node.
 
-class VisualGraph:
+class VisualGraph(Scene):
     
-    def __init__(self):
-        pass
-    def construct(self):
-        i = 0
-        RootNode = node(0)
-        self.new_node(RootNode, 0)
+    def __init__(self, **kwargs):
+        self.edges = []
+        self.vertexes = []
+        super().__init__(**kwargs)
         
-    def new_node(self, val, Vertex):
-        if val<8:
-            Vertex.ln = self.new_node(2**(val+1))
-            Vertex.rn = self.new_node(2**(val+2))
+
+    def construct(self):
+        rootNode = node(1)
+        self.new_node(1, rootNode)  # Populate the tree
+        self.traverse(rootNode) 
+        G = Graph(self.vertexes, self.edges, layout="tree", vertex_config= {"color": BLUE}, root_vertex=1,labels=True, label_fill_color=BLACK)
+        self.play(Create(G))
+        self.wait(2)
+        
+    def new_node(self, depth, Vertex):
+        if depth < 4:
+            Vertex.ln = node(2 * Vertex.val)
+            Vertex.rn = node(2 * Vertex.val + 1)
+            self.new_node(2*depth, Vertex.ln)
+            self.new_node(2*depth+1, Vertex.rn)
         else:
             return
     def traverse(self, RootNode):
             Node = RootNode
+            if Node.val not in self.vertexes:
+                self.vertexes.append(Node.val)
             print(Node)
-            if Node.ln != -1:
+            if Node.ln != None:
+                self.edges.append((Node.val, Node.ln.val))
                 self.traverse(Node.ln)
-            if Node.rn != -1:
+            if Node.rn != None:
+                self.edges.append((Node.val, Node.rn.val))
                 self.traverse(Node.rn)
-        
+    
+
 ##Need to add the graphics now
     
